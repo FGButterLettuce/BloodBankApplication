0834d7a71035cc895e8031289b8d9c7ffd280b52
8Cores.NoBite <niranjanrakesh64@gmail.com>
2019-04-09 22:36:27 +0400

    login&signup forms complete

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  constructor(private router:Router){
+
+  }
+
+signup(){
+  this.router.navigate(['sign-up']);
+}
+
+login(){
+  this.router.navigate(['log-in']);
+}
 
 }
